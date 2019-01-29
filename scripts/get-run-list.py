8387033634eebe74658beaebf640b86c8dc0db4f
Nathan Baltzell <baltzell@ifarm1402.jlab.org>
2019-01-29 07:32:58 -0500

    making run lists

--- scripts/get-run-list.py
+++ scripts/get-run-list.py
@@ -8,14 +8,21 @@ runEnd = int(sys.argv[2])
 
 rfgs = RunFileGroups()
 
+print 'adding runs ...'
 for run in range(runStart,runEnd+1):
+  print run
   rfgs.addRun(run)
 
 print 'loading files ...'
-for fileName in open('/home/baltzell/clas12/rga-spring.list','r').readlines():
+for fileName in open('/home/baltzell/clas12/rga/rga-spring-files.txt','r').readlines():
+  print fileName
   rfgs.addFile(fileName)
 
 rcdb = RcdbManager()
+
+for run,rfg in rfgs.rfgs.iteritems():
+  if rfg.size()>10:
+    print run,rfg.size()
 
 print 'checking rcdb ...'
 missing=[]
@@ -26,7 +33,6 @@ for run in rfgs.getRunList(10):
     r=rcdb.getRunStartTime(run)
   except AttributeError:
     missing.append(run)
-
 print missing
 
 
