89e78109ddfc424a6f9cd713a67c9fc64649390b
Nathan Baltzell <baltzell@ifarm1402.jlab.org>
2019-01-29 13:30:52 -0500

    let run file lists contain comments after 1st column

--- scripts/gen-decoding.py
+++ scripts/gen-decoding.py
@@ -30,7 +30,7 @@ workflow.addRuns(cfg['runs'])
 if os.path.isdir(cfg['inputs']):
   workflow.addDir(cfg['inputs'])
 elif os.path.isfile(cfg['inputs']):
-  workflow.addFiles(open(cfg['inputs'],'r').readlines())
+  workflow.addFiles([x.split()[0] for x in open(cfg['inputs'],'r').readlines()])
 else:
   sys.exit('ERROR:  inputs must be a file (containing a list of files) or a directory:\n'+cfg['inputs'])
 
