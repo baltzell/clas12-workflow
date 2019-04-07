f0d95120accb9f47e691f144c6aedd1f59765957
Nathan Baltzell <baltzell@ifarm1402.jlab.org>
2019-04-07 19:47:00 -0400

    fix quotes on example

--- examples/rga-decode-singles-glob-100sAnd200s.sh
+++ examples/rga-decode-singles-glob-100sAnd200s.sh
@@ -4,5 +4,5 @@
     --run 6688,6689 \
     --inputs /mss/clas12/rg-a/data/clas_00668[89] \
     --outDir /volatile/clas12/baltzell/test2 \
-    --fileRegex .*clas[_A-Za-z]*_(\d+)\.evio\.(0*[01]\d\d$)
+    --fileRegex '.*clas[_A-Za-z]*_(\d+)\.evio\.(0*[01]\d\d$)'
 
