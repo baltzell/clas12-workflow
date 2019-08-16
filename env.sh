8f4b1c6f3d7eb2769760f8d00c02fc5250eb2eea
Nathan Baltzell <baltzell@ifarm1402.jlab.org>
2019-08-15 20:24:07 -0400

    switch to modules for rcdb env setup

--- env.sh
+++ env.sh
@@ -1,10 +1,6 @@
 
-if [ -e /group/clas12/rcdb/environment.bash ]
-then
-    source /group/clas12/rcdb/environment.bash
-else
-    echo WARNING:  Cannot find RCDB installation.
-fi
+source /group/clas12/packages/setup.sh
+module load rcdb
 
 d="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH=${PYTHONPATH}:${d}/lib
