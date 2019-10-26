bd5ce2a4576aa1b8b186ac5e8f39318f28656dee
Nathan Baltzell <baltzell@ifarm1802.jlab.org>
2019-10-26 15:41:34 -0400

    more slurm-status options

--- scripts/slurm-status.py
+++ scripts/slurm-status.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
-import sys,argparse
+import sys,pwd,argparse,datetime
 
 from SlurmStatus import SlurmQuery
 
 cli=argparse.ArgumentParser(description='Query SLURM job status.')
-cli.add_argument('--user',metavar='NAME',help='user name', type=str, required=True)
-cli.add_argument('--days',metavar='#',help='# days to span', type=int, default=7)
-cli.add_argument('--end',metavar='DATE',help='end date of query span', type=str, default=None)
+cli.add_argument('users',metavar='user',help='username', type=str, nargs='+')
+cli.add_argument('--days',metavar='#',help='Number of days to span', type=int, default=7)
+cli.add_argument('--end',metavar='YYYY-MM-DD',help='End date of query span (at 24:00)', type=str, default=None)
 args=cli.parse_args(sys.argv[1:])
 
-sq=SlurmQuery(args.user)
-print sq.getTable()
+if args.end is not None:
+  try:
+    args.end=datetime.datetime.strptime(args.end,'%Y-%m-%d')
+    args.end+=datetime.timedelta(days=1)
+  except:
+    cli.error('Invalid date format:  '+args.end)
+
+for user in args.users:
+  try:
+    pwd.getpwnam(user)
+  except:
+    cli.error('User does not exist:  '+user)
+  sq=SlurmQuery(user)
+  sq.setDayDelta(args.days)
+  sq.setDayEnd(args.end)
+  print sq.getTable()
 
