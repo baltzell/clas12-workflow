ac1e2cec9910e7446e7286055ec2dc65f84c2246
Nathan Baltzell <baltzell@ifarm1901.jlab.org>
2019-12-20 19:19:34 -0500

    run/train merging scripts

--- scripts/hipo-merge-runs.py
+++ scripts/hipo-merge-runs.py
@@ -5,7 +5,8 @@ import ChefUtil
 from RunFileUtil import getRunList
 from RunFileUtil import RunFileGroups,RunFile
 
-# WARNING:  requires hipo-utils to be in $PATH
+# WARNING:  requires hipo-utils to be in $COATJAVA/bin else $PATH
+# FIXME:  check for hipo-utils and report error if can't find it
 
 logging.basicConfig(level=logging.INFO,format='%(levelname)-9s[%(name)-14s] %(message)s')
 logger=logging.getLogger(__name__)
@@ -13,26 +14,46 @@ logger=logging.getLogger(__name__)
 cli=argparse.ArgumentParser(description='Merge a directory of HIPO files by run number.')
 cli.add_argument('-i',metavar='string',help='input files specification (directory, glob, file containing list of files), repeatable',type=str,default=[],action='append',required=True)
 cli.add_argument('-o',metavar='path',help='output prefix (automatically suffixed with "_run#.hipo")',type=str,required=True)
-#cli.add_argument('-d',help='dry run',default=False,action='store_true')
+cli.add_argument('-a',help='allow and skip pre-existing outputs',default=False,action='store_true')
+cli.add_argument('-d',help='dry run',default=False,action='store_true')
 args=cli.parse_args(sys.argv[1:])
 
+# collect the input runs/files: 
 rfgs=RunFileGroups()
 rfgs.addRuns(getRunList(args.i))
 rfgs.findFiles(args.i)
 
 ChefUtil.mkdir(os.path.dirname(args.o))
 
+# if not allowing pre-existing outputs, abort ASAP if any exist:
+if not args.a:
+  for rfg in rfgs.getGroups():
+    rf=RunFile(rfg[0])
+    out=args.o+'_%.6d.hipo'%rf.runNumber
+    if os.path.exists(out):
+      logger.error('File already exists:  '+out)
+      sys.exit(1)
+
+if len(rfgs.getGroups())==0:
+  logger.error('Found no runs')
+else:
+  logger.info('Merging %d runs ...',len(rfgs.getGroups()))
+
+# run `hipo-utils -merge` once per run number:
 for rfg in rfgs.getGroups():
   rf=RunFile(rfg[0])
   out=args.o+'_%.6d.hipo'%rf.runNumber
-  if os.path.exists(out):
-    sys.exit('File already exists:  '+out)
-
-for rfg in rfgs.getGroups():
-  rf=RunFile(rfg[0])
-  out=args.o+'_%.5d.hipo'%rf.runNumber
-  cmd=['hipo-utils','-merge','-o',out]
+  if args.a and os.path.exists(out):
+    logger.warning('Skipping pre-existing output file:  '+out)
+    continue
+  hu='hipo-utils'
+  if os.getenv('COATJAVA') is not None:
+    hu=os.getenv('COATJAVA')+'/bin/hipo-utils'
+  cmd=[hu,'-merge','-o',out]
   cmd.extend(rfg)
+  if args.d:
+    print(cmd)
+    continue
   try:
     process=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     while True:
---
+++ scripts/hipo-merge-trains.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import re,os,sys,logging,subprocess,argparse
+
+logging.basicConfig(level=logging.INFO,format='%(levelname)-9s[%(name)-14s] %(message)s')
+logger=logging.getLogger(__name__)
+
+cli=argparse.ArgumentParser(description='Merge a directory of clas12-workflow train outputs.')
+cli.add_argument('-i',metavar='path',help='input directory',type=str,required=True)
+cli.add_argument('-o',metavar='path',help='output directory',type=str,required=True)
+args=cli.parse_args(sys.argv[1:])
+args.i=args.i.rstrip('/')
+
+if not os.path.isdir(args.i):
+  logger.error('-i must be an existing directory of input files')
+  sys.exit(-1)
+
+basepath=None
+singleRun=None
+trainIndices=set()
+
+# FIXME: this should be way less complicated
+# FIXME: maybe args should include run number list/range
+# FIXME: maybe should limit walk depth if that would simplify
+
+# check if basename of input directory is a run number:
+# (if so, we're going to limit to that run number)
+mm=re.match('^(\d+)$',os.path.basename(args.i))
+if mm is not None:
+  singleRun=int(mm.group(1))
+  logger.info('limiting to run '+str(singleRun))
+
+# walk the input directory, requiring valid clas12-workflow
+# train directory structure, ignoring/erroring otherwise:
+logger.info('scanning input directory for train files ...')
+for dirpath,dirnames,filenames in os.walk(args.i):
+  # check that it's a run-number directory:
+  print os.path.basename(dirpath)
+  mm=re.match('^(\d+)$',os.path.basename(dirpath))
+  if mm is None:
+    continue
+  # if limiting to one run number, ignore others:
+  if singleRun is not None and int(mm.group(1))!=singleRun:
+    logger.info('ignoring run '+mm.group(1))
+    continue
+  # get the "basepath", one up from this run number directory:
+  bp='/'+'/'.join(dirpath.split('/')[0:-1])
+  # check that it's still the same basepath:
+  if basepath is None:
+    basepath=bp
+  elif basepath!=bp:
+    logger.error('Found multiple basepaths:')
+    logger.error(basepath)
+    logger.error(bp)
+    sys.exit(3)
+  # accumulate wagon indices, assuming skim#_ prefix on HIPO files:
+  for filename in filenames:
+    mm=re.match('^skim(\d+)_.*\.hipo$',filename)
+    if mm is not None:
+      trainIndices.add(int(mm.group(1)))
+
+if len(trainIndices)==0:
+  logger.error('Found no train output files at '+args.i)
+  sys.exit(2)
+else:
+  logger.info('Found %d wagons, merging them by run number ...'%len(trainIndices))
+
+# call hipo-merge-runs.py once per trainIndex:
+for trainIndex in sorted(trainIndices):
+  logger.info('Merging wagon #'+str(trainIndex))
+  if singleRun:
+    inGlob='%s/%.6d/skim%d_*.hipo'%(basepath,singleRun,trainIndex)
+  else:
+    inGlob='%s/*/skim%d_*.hipo'%(basepath,trainIndex)
+  outStub='%s/skim%d/skim%d'%(args.o,trainIndex,trainIndex)
+  cmd=[os.path.dirname(os.path.realpath(__file__))+'/hipo-merge-runs.py']
+  cmd.extend(['-i',inGlob,'-o',outStub])
+  p=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+  while True:
+    line=p.stdout.readline().rstrip()
+    if not line:
+      break
+    print(line)
+
