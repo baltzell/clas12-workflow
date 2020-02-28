0cba8eb97987dc7b87c6e518c4b52fab3deb8083
Nathan Baltzell <baltzell@ifarm1901.jlab.org>
2020-02-27 19:12:29 -0500

    merge old full swif retrieval for clas12mon

--- lib/clas12/CLAS12SwifStatus.py
+++ lib/clas12/CLAS12SwifStatus.py
@@ -20,6 +20,7 @@ def getHeader(workflowName):
   return header
 
 class CLAS12SwifStatus(SwifStatus):
+
   def __init__(self,name,args):
     SwifStatus.__init__(self,name)
     self.args=args
@@ -44,8 +45,10 @@ class CLAS12SwifStatus(SwifStatus):
         self.dbauth=authFile.read().strip()
     except:
       pass
+
   def isPreviousComplete(self):
     return self.previous is not None and self.previous.isComplete()
+
   def saveStatus(self):
     mkdir(self.args.logdir+'/status/')
     with open(self.statusFilename.replace('.json','.txt'),'w') as statusFile:
@@ -55,17 +58,20 @@ class CLAS12SwifStatus(SwifStatus):
     with open(self.statusFilename,'w') as statusFile:
       statusFile.write(self.getPrettyJsonStatus())
       statusFile.close()
+
   def saveLog(self):
     mkdir(self.args.logdir+'/logs/')
     with open(self.logFilename,'a+') as logFile:
       logFile.write('\n'+self.getPrettyJsonStatus())
       logFile.close()
+
   def saveDetails(self):
     mkdir(self.args.logdir+'/details/')
     with open(self.detailsFilename,'w') as detailsFile:
       detailsFile.write(self.getPrettyJsonDetails())
       detailsFile.close()
-  def saveDatabase(self):
+
+  def saveDatabase(self,full=False):
     if self.dbauth is None:
       print 'Missing clas12mon credentials.'
       return
@@ -77,35 +83,18 @@ class CLAS12SwifStatus(SwifStatus):
       if data['run_group'] not in CHOICES['runGroup']:
         print 'Invalid workflow name for clas12mon:  '+self.name
         return
+      # this is expensive (slow), as it requires requesting
+      # full workflow status from Swif:
+      if full and not self.tagsMerged:
+        self.mergeTags()
       status=self.getPrunedStatus()
+      #status[0]['succeeded']=200
       # convert to json string, and strip off leading/trailing
       # square brackets for clas12mon:
       data['entry'] = json.dumps(status).lstrip('[').rstrip().rstrip(']')
       headers={'Authorization':self.dbauth}
       return requests.post(self.dburl,data=data,headers=headers)
-  def __saveDatabase(self):
-    # don't do this, requires full workflow details just to get rungroup
-    # and task, unnecessary load on Swif
-    pass
-    # pull all job tags for clas12mon:
-    if not self.tagsMerged:
-      self.mergeTags()
-    if self.dbauth is None:
-      print 'Missing credentials in $HOME/.clas12mon.auth'
-    else:
-      # pull out special tags for clas12mon:
-      task=self.removeTag('task')
-      run_group=self.removeTag('run_group')
-      # get status without any nulls:
-      status=self.getPrunedStatus()
-      # convert to json string, and strip off leading/trailing
-      # square brackets for clas12mon:
-      data={}
-      data['entry'] = json.dumps(status).lstrip('[').rstrip().rstrip(']')
-      data['run_group'] = run_group
-      data['task'] = task
-      headers={'Authorization':self.dbauth}
-      return requests.post(self.dburl,data=data,headers=headers)
+
   def moveJobLogs(self):
     workDir = self.getTagValue('workDir')
     if workDir is not None:
@@ -113,6 +102,7 @@ class CLAS12SwifStatus(SwifStatus):
       dest=workDir+'/farm_out/'+self.name
       mkdir(dest)
       subprocess.check_output(['mv','%s/%s*'%(src,workflow),dest])
+
 
 if __name__ == '__main__':
   import argparse
