5787e5fb3dcaa4552a5f8b26814d8b5e99331f35
Nathan Baltzell <baltzell@ifarm1901.jlab.org>
2020-08-27 09:55:42 -0400

    add 2-hour buffer on decoding jobs

--- lib/clas12/CLAS12Jobs.py
+++ lib/clas12/CLAS12Jobs.py
@@ -41,7 +41,7 @@ class DecodeAndMergeJob(CLAS12Job):
   def addInputData(self,eviofiles):
     # FIXME:  this assume 2 GB EVIO file
     self.setDisk('%.0fGB'%(int(ChefUtil.DEFAULT_EVIO_BYTES*1.4)/1e9*len(eviofiles)+1))
-    self.setTime('%.0fh'%(len(eviofiles)))
+    self.setTime('%.0fh'%(len(eviofiles)+2))
     decodedfiles=[]
     for eviofile in eviofiles:
       CLAS12Job.addInputData(self,eviofile)
