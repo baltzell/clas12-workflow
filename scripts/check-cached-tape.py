9dc8b9087c4656667645c65864f0e966e014028c
Nathan Baltzell <baltzell@ifarm1901.jlab.org>
2021-03-03 10:22:32 -0500

    cleanup cache/mss comparison

--- scripts/check-cached-tape.py
+++ scripts/check-cached-tape.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-
-import sys
 import os
-import zlib
-import hashlib
+import sys
+import logging
 import argparse
+
+import JLabTape
+
+logging.basicConfig(level=logging.INFO,format='%(levelname)-9s[ %(name)-15s ] %(message)s')
 
 cli = argparse.ArgumentParser('Compare /cache files with their /mss counterparts.')
 cli.add_argument('--crc32',help='compare crc32 checksums (slow)',default=False,action='store_true')
@@ -12,31 +14,10 @@ cli.add_argument('--md5',help='compare md5 checksums (slow)',default=False,actio
 cli.add_argument('path', nargs='+',help='path of directory or file on /cache (repeatable)')
 args = cli.parse_args(sys.argv[1:])
 
-class TapeStub():
-  def __init__(self,path):
-    self.path = path
-    with open(self.path,'r') as f:
-      for line in f.readlines():
-        key,val = line.strip().split('=')
-        if key == 'size':
-          self.size = int(val)
-        elif key == 'md5':
-          self.md5 = val
-        elif key == 'crc32':
-          self.crc32 = val
-
-def crc32(path):
-  ret = 0
-  with open(path,'rb') as f:
-    for line in f.readlines():
-      ret = zlib.crc32(line, ret)
-  return '%x'%ret
-
-def md5(path):
-  with open(path,'rb') as f:
-    md5 = hashlib.md5()
-    md5.update(f.read())
-    return str(md5.hexdigest())
+def check_cache_file(cache_path):
+  cache = JLabTape.CachedFile(cache_path, crc32=args.crc32, md5=args.md5)
+  tape = JLabTape.TapeStub('/mss'+cache_path[6:])
+  return cache == tape
 
 mismatch=[]
 
@@ -44,22 +25,17 @@ for x in args.path:
   if not x.startswith('/cache/'):
     print('Ignoring non-/cache path: '+x)
     continue
-  if not os.path.isfile(x) and not os.path.isdir(x):
-    print('Ignoring non-existiend path:  '+x)
+  if os.path.isfile(x):
+    if not check_cache_file(x):
+      mismatch.append(x)
+  elif os.path.isdir():
+    for adir,subdirs,files in os.walk(x):
+      for f in files:
+        if not check_cache_file(adir+'/'+f):
+          mismatch.append(adir+'/'+f)
+  else:
+    print('Ignoring non-existent path:  '+x)
     continue
-  for adir,subdirs,files in os.walk(x):
-    for f in files:
-      cache = adir+'/'+f
-      mss = '/mss'+cache[6:]
-      stub = TapeStub(mss)
-      if os.stat(cache).st_size != stub.size:
-        mismatch.append(cache)
-      elif os.stat(cache).st_size > 3e9:
-        print('Bypassing checksum on large file:  '+cache)
-      elif args.crc32 and stub.crc32 != crc32(cache):
-        mismatch.append(cache)
-      elif args.md5 and stub.md5 != md5(cache):
-        mismatch.append(cache)
 
 if len(mismatch) == 0:
   print('No Mismatches Found.')
