36fa45e1c955ea21f0338261dc1907ae0c3a9460
Nathan Baltzell <baltzell@gmx.com>
2021-11-19 09:12:09 -0500

    add kill option

--- scripts/slurm-exclude-nodes.sh
+++ scripts/slurm-exclude-nodes.sh
@@ -1,35 +1,70 @@
 #!/bin/bash
 
-usage="\nUsage:  slurm-exclude-nodes.sh NODELIST\n"
-usage="$usage\twhere NODELIST is a SLURM specification for nodes to exclude, e.g.\n"
-usage="$usage\t'farm[1909-1912]'.  This operates on all \$USERS's PENDING jobs.\n"
+USAGE () {
+    echo -e "\nUsage:  slurm-exclude.nodes.sh [-h] [-k] NODES\n"
+    echo -e "\t-h     print this usage"
+    echo -e "\t-k     kill jobs running on the specified nodes"
+    echo -e "\t-e     exclude the specified nodes from pending jobs"
+    echo -e "\tNODES  a SLURM specificiation for a list of nodes\n"
+    echo -e "Example of a SLURM node specification: 'farm[1909-1912]'\n"
+}
 
-if [ "$#" -ne 1 ]
+KILL=0
+EXCLUDE=0
+while getopts "khe" opt
+do
+    case "${opt}" in
+        k)
+            KILL=1
+            ;;
+        e)
+            EXCLUDE=1
+            ;;
+        *)
+            USAGE
+            exit
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [ $KILL -eq 0 ] && [ $EXCLUDE -eq 0 ]
 then
-    echo -e $usage
-    echo -e "ERROR: Exactly ONE argument is required, the NODELIST.\n"
+    echo "ERROR:  At least one of -k or -e must be specified."
+    USAGE
     exit
-elif [ "$1" = "-h" ]
+elif [ "$#" -ne 1 ]
 then
-    echo -e $usage
+    echo -e "ERROR:  Exactly 1 NODES must be specified."
+    USAGE
     exit
 fi
 
-ExcNodeList=$1
+NODES=$1
 
-# Seems we cannot just apply it to all jobs, but only via id/name,
-# (hence this convenience script), so let's get and loop over a list
-# of applicable jobs:
-for x in $(squeue --noheader -u $USER --states=PENDING -o '%A,%x')
-do
-    # check whether it already matches, just to speed
-    # this up and avoid unnecessary SLURM modifications:
-    jobid=$(echo $x | awk -F, '{print$1}')
-    exclu=$(echo $x | awk -F, '{print$2}')
-    if [ "$exclu" = "" ] || ! [ $exclu = $1 ]
-    then
-        echo "Excluding nodes for job #$jobid: $ExcNodeList"
-        scontrol update UserID=$USER JobId=$jobid ExcNodeList=$ExcNodeList
-    fi
-done
+if [ $KILL -eq 1 ]
+then
+    scancel -u $USER -w $NODES
+fi
+
+if [ $EXCLUDE -eq 1 ]
+then
+
+    # Seems we cannot just apply it to all jobs, but only via id/name,
+    # (hence this convenience script), so let's get and loop over a list
+    # of applicable jobs:
+    for x in $(squeue --noheader -u $USER --states=PENDING -o '%A,%x')
+    do
+        # check whether it already matches, just to speed
+        # this up and avoid unnecessary SLURM modifications:
+        jobid=$(echo $x | awk -F, '{print$1}')
+        exclu=$(echo $x | awk -F, '{print$2}')
+        if [ "$exclu" = "" ] || ! [ $exclu = $1 ]
+        then
+            echo "Excluding $NODES for job #$jobid"
+            scontrol update UserID=$USER JobId=$jobid ExcNodeList=$NODES
+        fi
+    done
+fi
 
