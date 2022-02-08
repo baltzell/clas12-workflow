2f068b8f2772623481165cd4d561036a1ad6ede2
Nathan Baltzell <baltzell@gmx.com>
2022-02-08 14:23:49 -0500

    add warning on unknown schema for disk request calculations

--- lib/clas12/ChefUtil.py
+++ lib/clas12/ChefUtil.py
@@ -54,7 +54,9 @@ def getReconFileBytes(schema,decodedfile):
   if   schema=='dst':   s *= 0.5
   elif schema=='calib': s *= 1.3
   elif schema=='mon':   s *= 1.6
-  else:                 s *= 4.0
+  else:
+    _LOGGER.warning('Schema unknown from schema_dir in YAML, because it doe not end in one of dst/calib/mon.  Disk request may be bad.')
+    s *= 4.0
   return s
 
 def getReconSeconds(decodedfile):
