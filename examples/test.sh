f358e84aea024aac448f211d32af9345d7df40af
Nathan Baltzell <baltzell@gmx.com>
2023-01-06 10:30:52 -0500

    add test

---
+++ examples/test.sh
@@ -0,0 +1,11 @@
+clas12-workflow.py \
+--model recana \
+--runGroup rga \
+--tag test \
+--runs 5196 \
+--inputs /mss/clas12/rg-a/production/decoded/6b.2.0/005196/ \
+--outDir /volatile/clas12/users/baltzell/wok-test \
+--reconYaml ./examples/test.yaml \
+--trainYaml calib \
+--coatjava 8.4.0  
+
