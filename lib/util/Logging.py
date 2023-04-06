7dbd87de462b9d0c296ca2f01d0f50f53d7bc9f7
Nathan Baltzell <baltzell@gmx.com>
2023-04-06 16:34:38 -0400

    add colored logger

---
+++ lib/util/Logging.py
@@ -0,0 +1,37 @@
+import logging
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg):
+        self.length = 20
+        self.colors = {'WARNING':34,'CRITICAL':31,'ERROR':31}
+        self.reset_seq = '\033[0m'
+        self.color_seq = '\033[1;%dm'
+        logging.Formatter.__init__(self, msg)
+    def format(self, record):
+        if record.levelname in self.colors:
+            l = record.levelname
+            record.levelname = '%-20s'%( (self.color_seq % self.colors[l]) + l + self.reset_seq)
+            record.msg =  self.color_seq % (self.colors['CRITICAL']) + record.msg + self.reset_seq
+        else:
+            l = self.length - len(self.reset_seq) - len(self.color_seq)
+            record.levelname = ('%%-%ds'%l) % record.levelname
+        return logging.Formatter.format(self, record)
+
+class ColoredLogger(logging.Logger):
+    message = '%(levelname)s[ %(name)-15s ] %(message)s'
+    def __init__(self, name):
+        logging.Logger.__init__(self, name, logging.INFO)
+        console = logging.StreamHandler()
+        console.setFormatter(ColoredFormatter(self.message))
+        self.addHandler(console)
+        return
+
+if len(logging._handlers)==0:
+  logging.setLoggerClass(ColoredLogger)
+
+
+#logger=logging.getLogger(__name__)
+#logger.info('info123')
+#logger.warning('warning123')
+#logger.critical('critical123456')
+
