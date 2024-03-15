8c9d814b047d55b33bb4dfa06950b4ae1d1507e7
Nathan Baltzell <baltzell@gmx.com>
2024-03-15 19:51:55 -0400

    kludge detect denoising environment

--- lib/clas12/scripts/denoise.sh
+++ lib/clas12/scripts/denoise.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-#
-# quick hack to get denoising validation in the workflow
-#
 
-[ -z $1 ] && v=1.9 || v=$1
+if grep -q -i Alma /etc/redhat-release
+then
+    d=/scigroup/cvmfs/hallb/clas12/soft/almalinux9-gcc11/local/driftchambers/4.0.1
+    exe=$d/bin/denoise2.exe
+    network=$d/share/denoising/network
+else
+    g=/apps/gcc/10.2.0
+    export PATH=${g}/bin:${PATH}
+    export LD_LIBRARY_PATH=${g}/lib64:${g}/lib:${PATH}
+    d=/group/clas12/packages/hipo/2.0/extensions/dc
+    exe=$d/denoise2.exe
+    network=$d/network
+fi
 
-d=/group/clas12/packages/hipo/$v/extensions/dc
-
-# requires this subdir in $PWD:
-ln -sf $d/network
-
-g=/apps/gcc/10.2.0
-export PATH=${g}/bin:${PATH}
-export LD_LIBRARY_PATH=${g}/lib64:${g}/lib:${PATH}
+# denoising requires $PWD/network
+ln -sf $network
 
 for x in `find . -maxdepth 1 -xtype f -name '*.hipo' | sed 's;^\./;;'`
 do
   date +'DENOISE START: %F %H:%M:%S'
-  $d/denoise2.exe -i $x -o dn_${x} -l 0.01 -t 16 
+  $exe -i $x -o dn_${x} -l 0.01 -t 16 
   [ $? -ne 0 ] && echo 'DENOISE ERROR' && exit 100
   date +'DENOISE STOP: %F %H:%M:%S'
   mv -f dn_${x} $x # replace the input file
