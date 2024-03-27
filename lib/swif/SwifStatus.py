d9825d066f36296bad0d4c96cecac5e489402cbf
Nathan Baltzell <baltzell@gmx.com>
2024-03-27 16:31:51 -0400

    new swif location on el9

--- lib/swif/SwifStatus.py
+++ lib/swif/SwifStatus.py
@@ -1,7 +1,7 @@
 import os,sys,glob,json,copy,subprocess,getpass,datetime,collections
 import FileUtil
 
-SWIF='/site/bin/swif2'
+SWIF='/usr/local/bin/swif2'
 
 _JSONFORMAT={'indent':2,'separators':(',',': '),'sort_keys':True}
 
