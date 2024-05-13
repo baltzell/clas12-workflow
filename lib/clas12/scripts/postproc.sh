3cfba703ba8f7ec556df0e8e22b77f3a31832089
Nathan Baltzell <baltzell@gmx.com>
2024-05-13 16:19:27 -0400

    cleanup postprocessing script

--- lib/clas12/scripts/postproc.sh
+++ lib/clas12/scripts/postproc.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-export JAVA_OPTS="$JAVA_OPTS -Djava.io.tmpdir=. -Dorg.sqlite.tmpdir=."
 usage="postproc.sh [-r] [-p] [-d] -o output input [input ...]]"
-
+export JAVA_OPTS="$JAVA_OPTS -Djava.io.tmpdir=. -Dorg.sqlite.tmpdir=."
 [ "x$CLARA_HOME" != "x" ] && bin=$CLARA_HOME/plugins/clas12/bin
-[ "x$COATJAVA" != "x" ] && [ "x$bin" != "x" ] && bin=$COATJAVA/bin
-
+[ "x$COATJAVA" != "x" ] && [ "x$bin" == "x" ] && bin=$COATJAVA/bin
 while getopts "rpdo:" OPTION; do
     case $OPTION in
         r)  recharge=1 ;;
@@ -16,20 +14,28 @@ while getopts "rpdo:" OPTION; do
 done
 shift $((OPTIND-1))
 input=$@
-
 [ "x$recharge" == "x" ] && [ "x$postproc" == "x" ] && echo $usage && echo ERROR:  at least one of -r/-p is required. && exit 1
 [ "x$output" == "x" ] && echo $usage && echo ERROR:  -o is required. && exit 1
 [ "x$input" == "x" ] && echo $usage && echo ERROR:  input file required. && exit 1
 
-if [ "x$recharge" != "x" ]; then 
-    $bin/rebuild-scalers -o $output $input
-    input=$output
-fi
-if [ "x$postprocess" != "x" ]; then
-    [ "$input" == "$output" ] && mv -f $output x.hipo && input=x.hipo
-    opts='-q 1'
-    [ "x$nodelay" != "x" ] || opts="$opts -d 1"
-    $bin/postprocess $opts -o $output $input
+set -x -e
+tmpfile=tmp.hipo
+dbg=
+trap 'rm -f $tmpfile' EXIT
+
+if [ "x$recharge" != "x" ]; then
+    if [ "x$postproc" != "x" ]; then
+        $dbg $bin/rebuild-scalers -o $tmpfile $input
+    else
+        $dbg $bin/rebuild-scalers -o $output $input
+    fi
+    input=$tmpfile
 fi
 
-$bin/hipo-utils -test $output || rm -f $output && exit 99
+if [ "x$postproc" != "x" ]; then
+    opts='-q 1'
+    [ "x$nodelay" != "x" ] || opts="$opts -d 1"
+    $dbg $bin/postprocess $opts -o $output $input
+fi
+
+$dbg $bin/hipo-utils -test $output || rm -f $output
