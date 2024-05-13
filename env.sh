4a656883baeda924cc18c4900b9aa6ccfde9ac76
Nathan Baltzell <baltzell@gmx.com>
2024-05-13 17:01:27 -0400

    simplify env helpers

--- env.sh
+++ env.sh
@@ -1,19 +1,4 @@
 
-# load dependencies:
-#shell_ext=$(ps -p $$ -ocomm= | sed -e 's;^.*\.;;g' -e 's;^bash$;sh;')
-#setup=/group/clas12/packages/setup.$shell_ext
-setup=/group/clas12/packages/setup.sh
-
-if [ -e $setup ]
-then
-    source $setup
-    module -s purge
-    module -s load clas12
-else
-    echo "WARNING:  Cannot find CLAS12 modules setup file $setup"
-fi
-
-# put clas12-workflow/lib in $PYTHONPATH
 d="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH=${d}/lib/clas12:${d}/lib/hps:${d}/lib/swif:${d}/lib/util:${d}/lib/ccdb:${PYTHONPATH}
 export PATH=${d}/scripts:${PATH}
