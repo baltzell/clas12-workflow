7af26775871d3a506fc723564c1d69f06970e36d
Nathan Baltzell <baltzell@gmx.com>
2024-06-20 18:10:11 -0400

    cleanup

--- scripts/farmout-cleanup.sh
+++ scripts/farmout-cleanup.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
-USAGE='farmout-cron.sh [-d #] [-z #]\n\t -d = delete older than # days\n\t -z = gzip older than # days'
-DIRECTORY=/farm_out/$USER
-PATH=/bin:/usr/bin
+function usage() {
+    x=$(basename $0)
+    echo -e "\nUsage: $x [-h] [-q] [-d #] [-z #]\n\t -d = delete older than # days\n\t -z = gzip older than # days\n"
+    exit $1
+}
 
-while getopts "d:z:" opt
+DIRECTORY=/farm_out/$USER
+export PATH=/bin:/usr/bin
+
+while getopts "d:z:qh" opt
 do
     case "${opt}" in
+        q)
+            QUIET=1
+            ;;
         d)
+            [ -z ${QUIET+x} ] || set -x
             find $DIRECTORY -mindepth 1 -type f -mtime +$OPTARG -delete 
             find $DIRECTORY -mindepth 1 -type d -mtime +$OPTARG -empty -delete 
             ;;
         z)
+            [ -z ${QUIET+x} ] || set -x
             find $DIRECTORY -mindepth 1 -type f -mtime +$OPTARG -not -name '*.gz' -exec gzip -f {} \;
             ;;
+        h)
+            usage 0
+            ;;
         *)
-            echo -e "\nUsage = $USAGE\n"
-            exit
+            usage 1
             ;;
     esac
 done
+
+if [ "$OPTIND" -eq 1 ]; then
+    echo "ERROR:  At least one of -d/-z is required."
+    usage 1
+fi
 
