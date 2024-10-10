38730e0e625ea743788d51f5c6c981fe03cd744e
Nathan Baltzell <baltzell@gmx.com>
2024-10-10 13:56:03 -0400

    update test script

--- examples/test.sh
+++ examples/test.sh
@@ -9,7 +9,7 @@ clas12-workflow \
 --inputs /mss/clas12/rg-k/data/clas_019892 \
 --inputs /mss/clas12/rg-k/data/clas_019893 \
 --outDir /volatile/clas12/users/baltzell/wok-test \
---reconYaml /home/baltzell/clas12-config/coatjava/10.0.5/rgk_data-cv-calib.yaml \
+--reconYaml ~/clas12-default.yaml \
 --trainYaml calib \
 --coatjava 10.1.0 \
 --denoise
