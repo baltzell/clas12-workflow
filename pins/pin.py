4cb49dc103c6e6b6b32ae9d614b3264dc3dfcf8f
Nathan Baltzell <baltzell@gmx.com>
2024-12-11 12:09:34 -0500

    cleanup

--- pins/pin.py
+++ pins/pin.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 import os,sys,glob
-sizes = {}
-print('<p>/cache Pinning for CLAS12<br>')
-print('<p>Paths lists from run group chefs and analysis coordinators are used to calculate disk volume with <a href=\'https://code.jlab.org/hallb/clas12/clas12-workflow/-/blob/main/scripts/pin.py?ref_type=heads\'>this</a>.<p>')
-print('<ul>')
-for f in glob.glob(os.path.dirname(os.path.realpath(__file__))+'/*.txt'):
-    k = f.split('/').pop().split('.').pop(0)
-    sizes[k] = 0
+
+def get_sizes(f):
     for path in open(f):
         if path.startswith('/cache'):
             path = '/mss' + path[6:]
@@ -14,9 +9,25 @@ for f in glob.glob(os.path.dirname(os.path.realpath(__file__))+'/*.txt'):
             for file in files:
                 for line in open(os.path.join(root, file)):
                     if line.startswith('size'):
-                        sizes[k] += int(line.strip().split('=').pop())
+                        yield int(line.strip().split('=').pop())
                         break
-    print('<li>%s:  %.1f TB</li>' % (k.upper(),sizes[k]/1e12))
-print('<li>TOTAL: %.1f TB</li>' % (sum(sizes.values())/1e12))
-print('</ul>')
-#rm -f ./pins/README.html && ./scripts/pin.py >& ./pins/README.html && rsync -avz --delete ./pins clas12@ifarm:/group/clas/www/clasweb/html/clas12offline/disk/cache
+
+top = os.path.dirname(os.path.realpath(__file__))
+
+with open(top+'/README.html','w') as o:
+    def write(line):
+        o.write(line)
+        print(line, flush=True)
+    write('<p>/cache Pinning for CLAS12<br>')
+    write(f'<p>Paths lists from run group chefs and analysis coordinators are used to calculate disk volume with <a href=\'https://code.jlab.org/hallb/clas12/clas12-workflow/-/blob/main/pins/pin.py\'>this</a>.<p>')
+    write('<ul>')
+    sizes = {}
+    for f in glob.glob(top+'/*.txt'):
+        k = f.split('/').pop().split('.').pop(0)
+        sizes[k] = sum(list(get_sizes(f)))
+        write('<li>%s:  %.1f TB</li>' % (k.upper(),sizes[k]/1e12))
+    write('<li>TOTAL: %.1f TB</li>' % (sum(sizes.values())/1e12))
+    write('</ul>')
+
+print(f'rsync -avz --delete {top} clas12@ifarm:/group/clas/www/clasweb/html/clas12offline/disk/cache')
+print('https://clasweb.jlab.org/clas12offline/disk/cache/pin')
