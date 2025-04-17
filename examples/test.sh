c4a066e444622667a76aac6de9202abe77c2a23a
Nathan Baltzell <baltzell@gmx.com>
2025-04-17 19:38:32 -0400

    read-only cache test

--- examples/test.sh
+++ examples/test.sh
@@ -8,8 +8,8 @@ clas12-workflow \
 --inputs /mss/clas12/rg-k/data/clas_019891 \
 --inputs /mss/clas12/rg-k/data/clas_019892 \
 --inputs /mss/clas12/rg-k/data/clas_019893 \
---outDir /cache/hallb/scratch/baltzell/roc-test \
---workDir /volatile/clas12/users/baltzell/roc-test-work \
+--outDir /volatile/clas12/users/baltzell/wok-out \
+--workDir /volatile/clas12/users/baltzell/wok-work \
 --reconYaml ~/clas12-default.yaml \
 --trainYaml calib \
 --coatjava 10.1.0 \
